9671df72a62750bc482e9f12cca1d0ca8a95b3b3
Jasper Hafkenscheid <jasper.hafkenscheid@wearespindle.com>
2017-11-23 09:59:29 +0100

    Bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
-version = '0.2.2'
+version = '0.3.0'
 
 setup(
     name='cacofonisk',
