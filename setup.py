aada68ea5e701a357cbc3cfbdb4cbe471f8c4507
Hans Adema <hans.adema@wearespindle.com>
2018-10-11 11:58:39 +0200

    Bump version to 0.6.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-version = '0.5.1'
+version = '0.6.0'
 
 setup(
     name='cacofonisk',
