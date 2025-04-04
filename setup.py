cbf271fd29f9407ff427e357c0f14f7f828c0477
Jasper Hafkenscheid <jasper.hafkenscheid@voys.nl>
2025-04-04 11:51:25 +0200

    Release 0.7.9

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-version = "0.7.8"
+version = "0.7.9"
 
 setup(
     name="cacofonisk",
