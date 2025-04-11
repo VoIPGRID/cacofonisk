4a347a8bb1690dc7937c368f035111df12be604d
cpoppema <cpoppema@gmail.com>
2025-04-11 14:04:32 +0200

    fix: support py3.10+

--- tests/replaytest.py
+++ tests/replaytest.py
@@ -1,5 +1,9 @@
-from collections import Iterable
 import os
+
+try:
+    from collections import Iterable
+except ImportError:
+    from collections.abc import Iterable
 
 from cacofonisk import BaseReporter
 from cacofonisk import EventHandler
